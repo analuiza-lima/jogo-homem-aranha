11904c0de608e4ffaa6d1fb52812304febb7e114
analuiza-lima <ana.lima19@estudante.ifms.edu.br>
2026-05-15 09:59:57 -0400

    Add files via upload

---
+++ HomemAranha/src/main/java/com/mycompany/homemaranha/TelaInicialController.java
@@ -0,0 +1,12 @@
+package com.mycompany.homemaranha;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class TelaInicialController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
